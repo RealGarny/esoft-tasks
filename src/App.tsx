31e4333a8006937b879f625bf16df696f0a6d19d
realgarny <aseev.nik1@yandex.ru>
2024-04-13 13:59:08 +0500

    added basic logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import Button from "./components/Button"
+import Container from "./components/Container"
+import Input from "./components/Input"
+import Text from "./components/Text"
+import Box from "./components/Box"
+
+interface CompItem {
+  id:number,
+  text:string,
+  percent:string,
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [isCompVisible, setIsCompVisible] = useState<boolean>(false);
+  const [compList, setCompList] = useState<CompItem[]>([]);
+
+  const [fText, setFText] = useState<string>("");
+  const [fPercent, setFPercent] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, state:(...args:any)=>void) => {
+    e.preventDefault();
+    state(e.target.value);
+  }
+
+  const addComp = (e: React.FormEvent) => {
+    e.preventDefault();
+    compList.push({id:compList.length, text:fText, percent:fPercent})
+    setFPercent("");
+    setFText("");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container maxW="md">
+          <div className="mt-2 px-2">
+            <div>
+              <Text text="Добавить компетенцию" size="md"/>
+              <form className="flex gap-2" onSubmit={addComp}>
+                <Input
+                  placeholder="Название"
+                  onChange={(e)=>{handleChange(e, setFText)}}
+                  value={fText}
+                />
+                <Input
+                  placeholder="Процент Освоения"
+                  onChange={(e)=>{handleChange(e, setFPercent)}}
+                  value={fPercent}
+                />
+                <Button className="bg-primary text-background">
+                  <Text text="Добавить" />
+                </Button>
+              </form>
+            </div>
+            <Button 
+              className={`${isCompVisible ? "bg-secondary bg-opacity-50 text-text" : "bg-primary text-background"} transition-colors ease-in-out`}
+              onClick={()=>{setIsCompVisible(p => !p)}}
+            >
+              <Text text={isCompVisible ? "Скрыть компетенции" : "Показать компетенции"} />
+            </Button>
+            { isCompVisible &&
+            <div>
+              {compList.map(i => <Box key={i.id}><Text text={i.text}/>  <Text text={i.percent}/></Box>)}
+            </div>
+            }
+          </div>
+      </Container>
     </>
   )
 }
