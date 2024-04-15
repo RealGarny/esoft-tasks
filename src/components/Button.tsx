51e0e32b7190577e6e0d236a9cf6f51c69d6a2c5
RealGarny <aseev.nik1@yandex.ru>
2024-04-15 11:25:52 +0500

    blank project

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ interface BProps extends ContainerProps {
 const Button = (props:BProps) => {
     return(
         <button 
-            className={`px-4 py-2 w-full font-bold text-center rounded-lg ${props.className}`}
+            className={`px-4 py-2 w-full font-semibold text-center rounded-lg ${props.className}`}
             onClick={props.onClick && props.onClick}
         >
             {props.children}
