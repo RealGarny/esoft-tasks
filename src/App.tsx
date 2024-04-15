55375bd7437c58f5be9d4245daba1b5c3f4054fa
RealGarny <aseev.nik1@yandex.ru>
2024-04-15 11:36:19 +0500

    cherry-pick feature/3 and added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Button from "./components/Button"
 import Container from "./components/Container"
 import Input from "./components/Input"
 import Text from "./components/Text"
-import Box from "./components/Box"
 
 interface CompItem {
   id:number,
@@ -34,37 +33,7 @@ function App() {
   return (
     <>
       <Container maxW="md">
-          <div className="mt-2 px-2">
-            <div>
-              <Text text="Добавить компетенцию" size="md"/>
-              <form className="flex gap-2" onSubmit={addComp}>
-                <Input
-                  placeholder="Название"
-                  onChange={(e)=>{handleChange(e, setFText)}}
-                  value={fText}
-                />
-                <Input
-                  placeholder="Процент Освоения"
-                  onChange={(e)=>{handleChange(e, setFPercent)}}
-                  value={fPercent}
-                />
-                <Button className="bg-primary text-background">
-                  <Text text="Добавить" />
-                </Button>
-              </form>
-            </div>
-            <Button 
-              className={`${isCompVisible ? "bg-secondary bg-opacity-50 text-text" : "bg-primary text-background"} transition-colors ease-in-out`}
-              onClick={()=>{setIsCompVisible(p => !p)}}
-            >
-              <Text text={isCompVisible ? "Скрыть компетенции" : "Показать компетенции"} />
-            </Button>
-            { isCompVisible &&
-            <div>
-              {compList.map(i => <Box key={i.id}><Text text={i.text}/>  <Text text={i.percent}/></Box>)}
-            </div>
-            }
-          </div>
+          <Text text="Test" />
       </Container>
     </>
   )
